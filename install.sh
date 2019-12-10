f7c72545343c0976395c852b966ee7667c905147
corenel <xxdsox@gmail.com>
2019-12-10 20:51:12 +0800

    Update installation script

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo apt install -y libgflags-dev libboost-dev swig
 mkdir build && cd build
 cmake .. -DBUILD_PYTHON_BINDINGS=ON 
 make
-ln -sfn $PWD/build/_line_detector.so $HOME/.local/lib/python3.6/site-packages/
-ln -sfn $PWD/build/line_detector.py $HOME/.local/lib/python3.6/site-packages/
+ln -sfn $PWD/_line_detector.so $HOME/.local/lib/python3.6/site-packages/
+ln -sfn $PWD/line_detector.py $HOME/.local/lib/python3.6/site-packages/
